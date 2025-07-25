94de10c42e5795e89431865291e0acbaa3cf5b56
Bui Nhat Duy <buinhatduykbhb.2000@gmail.com>
2025-07-26 03:58:54 +0700

    Cập nhật ngày 26/07/2025

---
+++ assets/minimap/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'adamant'
+game 'gta5'
+lua54 'yes'
+author 'Hoang Anh Gaming'
+description 'Minimap cũ'
+version '1.0.0'
+
+--[[client_script 'client.lua'
+data_file "SCALEFORM_DLC_FILE" "stream/int3232302352.gfx"
+
+files {
+	"stream/int3232302352.gfx"
+}]]
